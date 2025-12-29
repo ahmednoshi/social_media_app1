7be323c4afad88e3c01d289a3fdb065a272eb5f5
ahmednoshi <ahmednoshi717@gmail.com>
2025-12-29 22:15:23 +0200

    Remove dist folder and ignore it

--- dist/utils/otp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOtp = void 0;
-const generateOtp = () => {
-    return Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
-};
-exports.generateOtp = generateOtp;
